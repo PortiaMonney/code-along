e336ed20e08cd3d3ab7332e38e02292c65eb0868
Portia Monney <baabamonney38@gmail.com>
2022-03-23 13:51:49 -0800

    React component 3

--- Arthur/code-along/src/App.js
+++ Arthur/code-along/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import writers from "./writers";
+
+// import './App.css';
+// import writers from "./writers";
+import { useEffect, useState } from 'react';
 import { ProfileCard } from './ProfileCard';
 
 function App() {
+      const [writers, setWriters] = useState([]);
+
+      useEffect(()=> {
+        const getWriters = async ()=> {
+          const res = await fetch("/writers.json");
+          const data = await res.json();
+          setWriters(data);
+        };
+        getWriters();
+      }, []);
+
   return (
     <div >
       <h1>Writer Profile</h1>
       <div className="container">
-        {writers.map(writer => (
+        {writers.map((writer) => (
           <ProfileCard writer={writer} />
 
 
@@ -18,6 +30,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- Arthur/code-along/src/ProfileCard.js
+++ Arthur/code-along/src/ProfileCard.js
@@ -14,6 +14,6 @@ export const ProfileCard = ({ writer }) => {
                 <p>{writer.phone}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
