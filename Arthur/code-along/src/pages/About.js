de466214c5f8c70c54a5562b68e11f63eea934ad
Portia Monney <baabamonney38@gmail.com>
2022-04-04 14:09:33 -0800

    react component 8

---
+++ Arthur/code-along/src/pages/About.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+        ducimus accusantium illo obcaecati minima eum incidunt eligendi quisquam
+        esse, quo harum tenetur doloribus suscipit, beatae modi voluptate
+        voluptatem? Temporibus, possimus. Dolores iusto error soluta aspernatur.
+        Quasi ducimus debitis sit cum dignissimos fuga in quam perferendis
+        molestiae deleniti aut voluptate voluptas, quos id repellat cupiditate
+        architecto minima adipisci aliquid laboriosam aspernatur? Aut dolore
+        asperiores fugit quas, ipsum ducimus delectus quisquam odio ipsam
+        explicabo exercitationem dicta expedita dolor necessitatibus autem
+        tempora nostrum, dolorem quaerat nesciunt cupiditate sapiente eos sit
+        nemo itaque? Libero? Perspiciatis rem eligendi, earum debitis temporibus
+        possimus voluptatum rerum excepturi autem consectetur culpa expedita
+        libero dignissimos placeat id, porro recusandae sint dolorum sunt fugiat
+        a pariatur magni nobis animi. Minus. Veritatis dignissimos velit
+        similique est nam animi, eveniet consectetur unde minima cum quam et
+        eligendi tenetur nisi officiis ab ut ipsa, autem aspernatur sint
+        mollitia fugiat. Deleniti odio nihil fugiat.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+        ducimus accusantium illo obcaecati minima eum incidunt eligendi quisquam
+        esse, quo harum tenetur doloribus suscipit, beatae modi voluptate
+        voluptatem? Temporibus, possimus. Dolores iusto error soluta aspernatur.
+        Quasi ducimus debitis sit cum dignissimos fuga in quam perferendis
+        molestiae deleniti aut voluptate voluptas, quos id repellat cupiditate
+        architecto minima adipisci aliquid laboriosam aspernatur? Aut dolore
+        asperiores fugit quas, ipsum ducimus delectus quisquam odio ipsam
+        explicabo exercitationem dicta expedita dolor necessitatibus autem
+        tempora nostrum, dolorem quaerat nesciunt cupiditate sapiente eos sit
+        nemo itaque? Libero? Perspiciatis rem eligendi, earum debitis temporibus
+        possimus voluptatum rerum excepturi autem consectetur culpa expedita
+        libero dignissimos placeat id, porro recusandae sint dolorum sunt fugiat
+        a pariatur magni nobis animi. Minus. Veritatis dignissimos velit
+        similique est nam animi, eveniet consectetur unde minima cum quam et
+        eligendi tenetur nisi officiis ab ut ipsa, autem aspernatur sint
+        mollitia fugiat. Deleniti odio nihil fugiat.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+        ducimus accusantium illo obcaecati minima eum incidunt eligendi quisquam
+        esse, quo harum tenetur doloribus suscipit, beatae modi voluptate
+        voluptatem? Temporibus, possimus. Dolores iusto error soluta aspernatur.
+        Quasi ducimus debitis sit cum dignissimos fuga in quam perferendis
+        molestiae deleniti aut voluptate voluptas, quos id repellat cupiditate
+        architecto minima adipisci aliquid laboriosam aspernatur? Aut dolore
+        asperiores fugit quas, ipsum ducimus delectus quisquam odio ipsam
+        explicabo exercitationem dicta expedita dolor necessitatibus autem
+        tempora nostrum, dolorem quaerat nesciunt cupiditate sapiente eos sit
+        nemo itaque? Libero? Perspiciatis rem eligendi, earum debitis temporibus
+        possimus voluptatum rerum excepturi autem consectetur culpa expedita
+        libero dignissimos placeat id, porro recusandae sint dolorum sunt fugiat
+        a pariatur magni nobis animi. Minus. Veritatis dignissimos velit
+        similique est nam animi, eveniet consectetur unde minima cum quam et
+        eligendi tenetur nisi officiis ab ut ipsa, autem aspernatur sint
+        mollitia fugiat. Deleniti odio nihil fugiat.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+        ducimus accusantium illo obcaecati minima eum incidunt eligendi quisquam
+        esse, quo harum tenetur doloribus suscipit, beatae modi voluptate
+        voluptatem? Temporibus, possimus. Dolores iusto error soluta aspernatur.
+        Quasi ducimus debitis sit cum dignissimos fuga in quam perferendis
+        molestiae deleniti aut voluptate voluptas, quos id repellat cupiditate
+        architecto minima adipisci aliquid laboriosam aspernatur? Aut dolore
+        asperiores fugit quas, ipsum ducimus delectus quisquam odio ipsam
+        explicabo exercitationem dicta expedita dolor necessitatibus autem
+        tempora nostrum, dolorem quaerat nesciunt cupiditate sapiente eos sit
+        nemo itaque? Libero? Perspiciatis rem eligendi, earum debitis temporibus
+        possimus voluptatum rerum excepturi autem consectetur culpa expedita
+        libero dignissimos placeat id, porro recusandae sint dolorum sunt fugiat
+        a pariatur magni nobis animi. Minus. Veritatis dignissimos velit
+        similique est nam animi, eveniet consectetur unde minima cum quam et
+        eligendi tenetur nisi officiis ab ut ipsa, autem aspernatur sint
+        mollitia fugiat. Deleniti odio nihil fugiat.
+      </p>
+    </section>
+  );
+};
+
+export default About;
