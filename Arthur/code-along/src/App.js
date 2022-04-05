2966453557ea636c580ac2f96947f092f373eafe
Portia Monney <baabamonney38@gmail.com>
2022-04-05 14:11:48 -0800

    react component 9

--- Arthur/code-along/src/App.js
+++ Arthur/code-along/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
+import BlogDetail from "./pages/BlogDetail";
 import Home from "./pages/Home";
+import BlogList from "./pages/BlogList";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="about" element={<About />} />
-        <Route path="blog" element={<Blog />} />
         <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="blog" element={<Blog />}>
+          <Route path="" element={<BlogList />} />
+          <Route path=":blog" element={<BlogDetail />} />
+        </Route>
+        {/* <Route path="/" element={<Home />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- Arthur/code-along/src/styles/Blog.js
+++ Arthur/code-along/src/styles/Blog.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-
+import withLayout from "../withLayout";
 
 function Blog() {
   return <Outlet />;
